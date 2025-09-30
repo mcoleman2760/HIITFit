c940f71b01518303b950fc64b2facbd585ebc46c
Mike <themcgamer9@gmail.com>
2025-09-30 19:44:47 -0400

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Reginald Coleman on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
